aade226ba920454a721f1d2c26ccd459dad6e6a5
Art3mis <adaka.office@gmail.com>
2020-04-20 17:14:09 +0200

    [PRG-01.1][PRG-02.1][PRG-02.2] Mainheader, Menü und Fontsanpassung
    Navbar korrigiert, Logoheader mit temp. Logo implementiert, Slideshow anstelle der Staticdarstellung, BG-Color Implementation, Resize Optimierung

--- index.php
+++ index.php
@@ -17,9 +17,65 @@
 	<title>Greenbike Graz</title>
 	</head>
 	<body>
-		<div class="jumbotron text-center">
-			<h1>E-BIKES - DIE NEUE ZUKUNFT</h1>
+		<!-- HEADER PREDIV -->
+		<div id="preheader">
 		</div>
+		<!-- LOGO HEADER -->
+		<div class="col-sm-12" id="logoheader">
+		</div>
+		<!-- SLIDER CAROUSEL -->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="pictures/slideshow1.jpg" class="carousel-img" alt="xxx">
+				</div>
+
+				<div class="item">
+					<img src="pictures/slideshow2.jpg" class="carousel-img" alt="xxx">
+				</div>
+
+				<div class="item">
+					<img src="pictures/slideshow3.jpg" class="carousel-img" alt="xxx">
+				</div>
+			</div>
+
+			<!-- Left and right controls 
+			<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			</a>-->
+		</div>
+		<!-- NAVBAR -->
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="bg-dark container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="#"><h1>ÜBER UNS</h1></a></li>
+						<li><a href="#"><h1>LEISTUNGEN</h1></a></li>
+						<li><a href="#"><h1>PRODUKTE</h1></a></li>
+						<li><a href="#"><h1>KONTAKT</h1></a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 
 		<div class="container">
 			<div class="row">
@@ -36,6 +92,7 @@
 					<p>Lorem ipsum dolor..</p>
 				</div>
 			</div>
-		</div> 
+		</div>
+		<br>
 	</body>
 </html>
