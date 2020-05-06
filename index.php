de7546de2886f6196cfbd9bc8d47001133a2f737
Art3mis <adaka.office@gmail.com>
2020-05-06 11:45:58 +0200

    [PRG-02.5] Sektion "Produkte" mit Produktslider und [BUG-2] Verknüpfung Titelleiste zur "Über Uns"
    + Spacing und Class "textarea"

--- index.php
+++ index.php
@@ -70,16 +70,16 @@
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="#">
+					<li><a href="#aboutus">
 							<h1>ÜBER UNS</h1>
 						</a></li>
 					<li><a href="#leistungen">
 							<h1>LEISTUNGEN</h1>
 						</a></li>
-					<li><a href="#">
+					<li><a href="#produkte">
 							<h1>PRODUKTE</h1>
 						</a></li>
-					<li><a href="#">
+					<li><a href="#kontakt">
 							<h1>KONTAKT</h1>
 						</a></li>
 				</ul>
@@ -89,10 +89,12 @@
 
 	<div class="container bg-light">
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-12" id="aboutus">
 				<img src="pictures/aboutus.jpg" class="max-wh">
-				<h2>ÜBER UNS</h2>
-				<p>Das ist Greenbike - ein junges Unternehmen mit speziell für E-Bikes ausgebildeten Fachkräften, die sich gerne für Sie Zeit nehmen. Unser Betrieb zeichnet sich durch die persönliche Beratung in unserem Fahrrad-Shop, der praktischen und kostengünstigen Vermietung von E-Bikes und unserer Fachwerkstätte für E-Bike Wartung und Service aus. Frei nach unserem Leitsatz "Greenbike - Schnell und Günstig" gestaltet sich unser Geschäftsmodell. Flexibilität für jedermann, direkt im Univiertel, praktische Erreichbarkeit mit öffentlichen Verkehrsmitteln, sowie Parkmöglichkeiten sind vorhanden. Die Greenbike GmbH beschäftigt derzeit vier Mitarbeiter, die sich kompetent und zuverlässig um alle Ihre Fragen und Anliegen rund um das Thema E-Bike kümmern.</p>
+				<div class="textarea">
+					<h2>ÜBER UNS</h2>
+					<p>Das ist Greenbike - ein junges Unternehmen mit speziell für E-Bikes ausgebildeten Fachkräften, die sich gerne für Sie Zeit nehmen. Unser Betrieb zeichnet sich durch die persönliche Beratung in unserem Fahrrad-Shop, der praktischen und kostengünstigen Vermietung von E-Bikes und unserer Fachwerkstätte für E-Bike Wartung und Service aus. Frei nach unserem Leitsatz "Greenbike - Schnell und Günstig" gestaltet sich unser Geschäftsmodell. Flexibilität für jedermann, direkt im Univiertel, praktische Erreichbarkeit mit öffentlichen Verkehrsmitteln, sowie Parkmöglichkeiten sind vorhanden. Die Greenbike GmbH beschäftigt derzeit vier Mitarbeiter, die sich kompetent und zuverlässig um alle Ihre Fragen und Anliegen rund um das Thema E-Bike kümmern.</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -136,58 +138,104 @@
 					<p class="p-light">Unsere Vermietung spricht für sich, indem wir Flexibilität zu geringen Preisen mit speziellen Preisvorteilen verbinden. Sie können aus einer großen Auswahl an moderensten E-Bikes aus unserem weiten Produktsortiment wählen. Wir beraten Sie gerne, ihr Wunsch-E-Bike zum besten Preis für Sie zu finden.</p>
 					<h3 class="h3-light">VERKAUF</h3>
 					<p class="p-light">Mit unseren generalüberholten und hochqualitativen E-Bike aus zweiter Hand erwerben Sie ein topaktuelles, neuwertiges E-Bike von Spitzenherstellern mit Bestpreisgarantie. Unsere E-Bikes werden von unserem hauseigenen Serviepersonal gewartet und geprüft, bevor Sie diese zum Vorzugspreis kaufen können. Wir bieten Ihnen auch die Möglichkeit, Ihr Wunsch-Rad vor dem Kauf Probe zu fahren, um sich so von der Qualtiät unserer Fahrräder zu überzeugen. Umweltfreundliche Fortbewegung, fachmännische Beratung, beste Preise - dass erhalten Sie nur bei Greenbike.</p>
+					<br>
 				</div>
 			</div>
-
 		</div>
 	</div>
-		<br>
-
-		<!--Studentenangebot-section -->
-		<div class="container bg-dark" id="studenten">
-			<div class="col-sm-12" style="margin-bottom: 10%">
-				<img id="stud-img" src="pictures/Studentenkarte.jpg">
-				<h3 class="h3-light">LIEBE SCHÜLER/INNEN UND STUDENTEN/INNEN, AUFGEPASST!</h3>
-			<p class="p-light">Wir bei Greenbike legen einen großen Wert auf Jugendliche, daher haben wir verschiedene Angebot für all diejenigen die Nachweisen können das sie studieren beziehungsweise in die Schule gehen. Sie bekommen dann eine Greenbike Karte mit dem man verschiedene E-Bikes günstiger mieten kann. Greenbike Mitglieder können auch bei verschiedenen Events mitmachen die über das ganze Jahr stattfinden und tolle Preise gewinnen!</p>
-			<p class="p-light"> Für mehr Fragen wenden Sie sich bei uns per E-Mail oder rufen Sie uns an!</p>
-			</div>
-		<div id="container-tab">
-			<table id="stud-tab">
-				<tr>
-					<th id="tab-head" colspan="3">Greenbike Schüler/Studenten Angebot</th>
+	<br>
+	<br>
+	<div class="container bg-light">
+		<div class="row">
+			<div class="col-sm-12" id="produkte">
+				<img src="pictures/Greenbike-Location.jpg" class="max-wh">
+				<div class="textarea">
+					<h2>PRODUKTE</h2>
+					<p>Wenn Sie in Graz unterwegs sind, denken Sie nicht, dass es hier nur öffentliche Verkehrsmittel gibt. Das E-Bike ist umweltfreundlich und bringt Ihnen viel Freude. Egal ob Sie überlegen, ein E-Bike zu kaufen oder einfach Ihren Urlaubsort auf eine ganz neue Art erkunden möchten - wir haben das passende E-Bike für Sie! Bei uns erwarten Sie eine Vielzahl an hochwertigen Marken E-Bikes. Wenn Sie sich unschlüssig sind, welches E-Bike am besten zu Ihnen passt, zögern Sie nicht uns zu kontaktieren. Wir helfen Ihnen gerne! Um sicherzustellen, dass wir an ihrem Wunschtermin das richtige Bike für Sie haben, rufen Sie uns an oder besuchen Sie uns in der Merangasse 82.</p>
 					
-				</tr>
-				<tr>
-					<td colspan="3" style="background-color: #5E6966"> </td>
-				</tr>
-				<tr>
-					<th>E-Bike</th>
-					<th>Normalpreis</th>
-					<th>Angebot</th>
-				</tr>
-				<tr>
-					<td>CT Hybrid One</td>
-					<td>€ ??,??</td>
-					<td>€ ??,??</td>
-				</tr>
-				<tr>
-					<td>S Bear Rock</td>
-					<td>€ ??,??</td>
-					<td>€ ??,??</td>
-				</tr>
-				<tr>
-					<td>B Wave</td>
-					<td>€ ??,??</td>
-					<td>€ ??,??</td>
-				</tr>
-			</table>
+
+
+					<h3>Unsere Empfehlungen für Sie:</h3>
+					<div id="carousel_products" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#carousel_products" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel_products" data-slide-to="1"></li>
+							<li data-target="#carousel_products" data-slide-to="2"></li>
+						</ol>
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+							<div class="item active">
+								<img src="pictures/Bike1slideshow.jpg" class="carousel-img" alt="xxx">
+							</div>
+
+							<div class="item">
+								<img src="pictures/Bike2slideshow.jpg" class="carousel-img" alt="xxx">
+							</div>
+
+							<div class="item">
+								<img src="pictures/Bike3slideshow.jpg" class="carousel-img" alt="xxx">
+							</div>
+						</div>
+
+						<a class="left carousel-control" href="#carousel_products" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#carousel_products" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+				<br>
+			</div>
 		</div>
+	</div>
+	<br>
+	
+	<!--Studentenangebot-section -->
+	<div class="container bg-dark" id="studenten">
+		<div class="col-sm-12" style="margin-bottom: 10%">
+			<img id="stud-img" src="pictures/Studentenkarte.jpg">
+			<h3 class="h3-light">LIEBE SCHÜLER/INNEN UND STUDENTEN/INNEN, AUFGEPASST!</h3>
+		<p class="p-light">Wir bei Greenbike legen einen großen Wert auf Jugendliche, daher haben wir verschiedene Angebot für all diejenigen die Nachweisen können das sie studieren beziehungsweise in die Schule gehen. Sie bekommen dann eine Greenbike Karte mit dem man verschiedene E-Bikes günstiger mieten kann. Greenbike Mitglieder können auch bei verschiedenen Events mitmachen die über das ganze Jahr stattfinden und tolle Preise gewinnen!</p>
+		<p class="p-light"> Für mehr Fragen wenden Sie sich bei uns per E-Mail oder rufen Sie uns an!</p>
 		</div>
-		<br>
-
-
-
-
+	<div id="container-tab">
+		<table id="stud-tab">
+			<tr>
+				<th id="tab-head" colspan="3">Greenbike Schüler/Studenten Angebot</th>
+				
+			</tr>
+			<tr>
+				<td colspan="3" style="background-color: #5E6966"> </td>
+			</tr>
+			<tr>
+				<th>E-Bike</th>
+				<th>Normalpreis</th>
+				<th>Angebot</th>
+			</tr>
+			<tr>
+				<td>CT Hybrid One</td>
+				<td>€ ??,??</td>
+				<td>€ ??,??</td>
+			</tr>
+			<tr>
+				<td>S Bear Rock</td>
+				<td>€ ??,??</td>
+				<td>€ ??,??</td>
+			</tr>
+			<tr>
+				<td>B Wave</td>
+				<td>€ ??,??</td>
+				<td>€ ??,??</td>
+			</tr>
+		</table>
+	</div>
+	</div>
+	<br>
 </body>
 
 </html>
